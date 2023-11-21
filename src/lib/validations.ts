165eae5c6a8457ea0eb5cf62e3992c9d94f05461
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-11-21 18:38:44 +0500

    registration done

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -3,5 +3,5 @@ import { z } from 'zod';
 export const signUpValidations = z.object({
   name: z.string().min(1, 'Enter your name'),
   email: z.string().min(1, 'Enter your Email').email('Enter a valid email'),
-  password: z.string().min(5, 'Password must be above 5 characters'),
+  password: z.string().min(6, 'Password must be above 5 characters'),
 });
